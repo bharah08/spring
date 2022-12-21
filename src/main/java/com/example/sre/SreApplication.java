7d8466995111e92401b18cc9085e6e0fa6549e0f
bharah08 <cxx7148@gmail.com>
2022-12-21 16:07:22 +0530

    hello

---
+++ src/main/java/com/example/sre/SreApplication.java
@@ -0,0 +1,13 @@
+package com.example.sre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sre/SreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
